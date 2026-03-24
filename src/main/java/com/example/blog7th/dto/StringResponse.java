9466eb21f8b2a60946776d466943a089c0a87ae5
lsh <lsh0131pianist@gmail.com>
2026-03-24 09:08:53 +0900

    [feat] 요청/응답 DTO 추가

---
+++ src/main/java/com/example/blog7th/dto/StringResponse.java
@@ -0,0 +1,13 @@
+package com.example.blog7th.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
+public class StringResponse {
+
+    private String string_one;
+    private String string_two;
+}
