9a38c4e53364288ba626e538c00470a0d19b5ed9
sky-0131 <lsh0131pianist@gmail.com>
2026-04-06 15:14:28 +0900

    [refactor]Jpa 설정 별도 클래스로 분리

--- src/main/java/com/example/blog7th/config/SecurityConfig.java
+++ src/main/java/com/example/blog7th/config/SecurityConfig.java
@@ -1,11 +1,9 @@
 package com.example.blog7th.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; // 비번 암호화용
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
